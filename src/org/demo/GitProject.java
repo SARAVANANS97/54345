d74aff92b3a024a07cd493232df01973b539eef3
SARAVANANS97 <saravanan6254@gmail.com>
2024-04-08 14:15:55 +0530

    work done

---
+++ src/org/demo/GitProject.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+public class GitProject {
+
+	public static void main(String[] args) {
+		
+System.out.println("git");
+System.out.println("push");
+System.out.println("pull");
+System.out.println("master");
+System.out.println("orgin");
+
+	}
+
+}
